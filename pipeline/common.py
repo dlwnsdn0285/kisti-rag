9f5705e9de5c222dfc430e080c29e90f99ec934d
dododadadada <minjeong19@naver.com>
2025-11-26 08:11:52 +0000

    revise path

--- pipeline/common.py
+++ pipeline/common.py
@@ -20,9 +20,9 @@ input_path = 'results/generated_answer.json'
 output_path = 'results/evaluation.json'
 input_path_ragchecker = 'multidim_evaluation/data_formatted/generated_answer.json'
 # pre-trained model path for recomp contriever and ext2gen generation models
-recomp_contriever_path = 'train/output/train_bi-encoder-mnrl-facebook-mcontriever-msmarco-2025-09-22_12-31-13'
-ext2gen_qwen7b_path = 'qwen7b_dpo_final_total_naive_prompt'
-ext2gen_koni4b_path = 'koni4b_dpo_final_total_naive_prompt'
+recomp_contriever_path = 'train/train_bi-encoder-mnrl-facebook-mcontriever-msmarco-2025-09-22_12-31-13'
+ext2gen_qwen7b_path = 'train/qwen7b_dpo_final_total_naive_prompt'
+ext2gen_koni4b_path = 'train/koni4b_dpo_final_total_naive_prompt'
 
 # Initialize Kiwi
 kiwi = Kiwi()
